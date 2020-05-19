3867e770fd809c6ef55b9f73d555aa74746207fc
Camilo Ortiz <camiloorvi@gmail.com>
2020-05-19 16:19:37 -0500

    Se agregan cards al home, css cards, js cards, carpeta img

--- js/script.js
+++ js/script.js
@@ -7,4 +7,56 @@ function toggleColumn(n) {
         } 
 }
 
+var clic = 1;
 
+function mostrar(clicked_id){
+     
+     /*
+     if(clic==1 && clicked_id=="git"){
+          document.getElementById('post').style.display="block";
+          clic = clic + 1;
+      }
+     else{
+          document.getElementById('post').style.display="none";
+          clic = 1;
+     }
+     */
+
+     if(clic==1 && clicked_id=="buttonGit"){
+          document.getElementById('git').style.display="block";
+          clic = clic + 1;
+     }
+     else{
+          document.getElementById('git').style.display="none";
+          clic = 1;
+     }
+
+     if(clic==1 && clicked_id=="buttonGitHub"){
+          document.getElementById('github').style.display="block";
+          clic = clic + 1;
+     }
+     else{
+          document.getElementById('github').style.display="none";
+          clic = 1;
+     }
+     
+     if(clic==1 && clicked_id=="buttonSsh"){
+          document.getElementById('ssh').style.display="block";
+          clic = clic + 1;
+     }
+     else{
+          document.getElementById('ssh').style.display="none";
+          clic = 1;
+     }
+
+     if(clic==1 && clicked_id=="buttonTag"){
+          document.getElementById('tags').style.display="block";
+          clic = clic + 1;
+     }
+     else{
+          document.getElementById('tags').style.display="none";
+          clic = 1;
+     }
+
+          
+}
