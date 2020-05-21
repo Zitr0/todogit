c084fd14487b67d13e94b1f9a18311cf63637c12
Camilo Ortiz <camiloorvi@gmail.com>
2020-05-21 15:27:28 -0500

    Se modifica js para cada card

--- js/script.js
+++ js/script.js
@@ -10,42 +10,79 @@ function toggleColumn(n) {
 var clic = 1;
 
 function mostrar(clicked_id){
+
+/*Visualizar Git*/     
+     if(clic==1 && clicked_id=="buttonGit"){     
+
+          if(clic==1 && document.getElementById('git').style.display=="none"){
+               
+               document.getElementById('git').style.display="block";
+               document.getElementById('github').style.display="none";
+               document.getElementById('ssh').style.display="none";
+               document.getElementById('tags').style.display="none";
+               clic = clic + 1;
+
+          }else{
+               document.getElementById('git').style.display="none";
+               clic = 1;
+          }
+
+     }
+
+/*Visualizar GitHub*/         
+     else if(clic==1 && clicked_id=="buttonGitHub"){
+
+          if(clic==1 && document.getElementById('github').style.display=="none"){
+               
+               document.getElementById('github').style.display="block";
+               document.getElementById('git').style.display="none";
+               document.getElementById('ssh').style.display="none";
+               document.getElementById('tags').style.display="none";
+               clic = clic + 1;
+
+          }else{
+               document.getElementById('github').style.display="none";
+               clic = 1;
+          }
+     }
+
+/*Visualizar Ssh*/    
+     else if(clic==1 && clicked_id=="buttonSsh"){
+        
+          if(clic==1 && document.getElementById('ssh').style.display=="none"){
+               
+               document.getElementById('ssh').style.display="block";
+               document.getElementById('git').style.display="none";
+               document.getElementById('github').style.display="none";
+               document.getElementById('tags').style.display="none";
+               clic = clic + 1;
+
+          }else{
+               document.getElementById('ssh').style.display="none";
+               clic = 1;
+          }
+     }
+
+/*Visualizar Tags*/         
+     else if(clic==1 && clicked_id=="buttonTag"){
+
+          if(clic==1 && document.getElementById('tags').style.display=="none"){
+               
+               document.getElementById('tags').style.display="block";
+               document.getElementById('git').style.display="none";
+               document.getElementById('github').style.display="none";
+               document.getElementById('ssh').style.display="none";
+               clic = clic + 1;
+
+          }else{
+               document.getElementById('tags').style.display="none";
+               clic = 1;
+          }
+     }
+
+     else{
+   
+          clic = 1;
+     }
      
-     if(clic==1 && clicked_id=="buttonGit"){
-          document.getElementById('git').style.display="block";
-          clic = clic + 1;
-     }
-     else{
-          document.getElementById('git').style.display="none";
-          clic = 1;
-     }
-
-     if(clic==1 && clicked_id=="buttonGitHub"){
-          document.getElementById('github').style.display="block";
-          clic = clic + 1;
-     }
-     else{
-          document.getElementById('github').style.display="none";
-          clic = 1;
-     }
-     
-     if(clic==1 && clicked_id=="buttonSsh"){
-          document.getElementById('ssh').style.display="block";
-          clic = clic + 1;
-     }
-     else{
-          document.getElementById('ssh').style.display="none";
-          clic = 1;
-     }
-
-     if(clic==1 && clicked_id=="buttonTag"){
-          document.getElementById('tags').style.display="block";
-          clic = clic + 1;
-     }
-     else{
-          document.getElementById('tags').style.display="none";
-          clic = 1;
-     }
-
-          
 }
